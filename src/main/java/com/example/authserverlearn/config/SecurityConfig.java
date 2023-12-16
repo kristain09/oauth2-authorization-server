361f6e4c357bc60d2cefef0a41419f9bd9ce1420
kristainputra <kristainputra98@gmail.com>
2023-12-16 20:45:36 +0700

    Oauth2 security filter chain configurer default

--- src/main/java/com/example/authserverlearn/config/SecurityConfig.java
+++ src/main/java/com/example/authserverlearn/config/SecurityConfig.java
@@ -38,129 +38,23 @@ import java.security.interfaces.RSAPublicKey;
 import java.util.UUID;
 
 
-//minimal configuration for getting started quickly
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-
-    @Bean
     @Order(1)
-    // for protocol endpoints
-    /*
-    * OAuth2AuthorizationServerConfiguration provides minimal default configuration for an Oauth2 authorization server
-    * make OAuth2AuthorizationServerConfigurer untuk mengimplementasikan default configuration dan meregistrasikan
-    * SecurityFilterChain, intinyanya yang applyDefaultSecurity ke httpSecurity.
-    * */
-    public SecurityFilterChain authorizeServerSecurityFilterChain(HttpSecurity http)
-        throws Exception {
-        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-        // initializing `OidcConfigurer -> OpenID Connect 1.0
-        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-                .oidc(Customizer.withDefaults());
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
 
-//        The OpenID Connect 1.0 Client Registration endpoint is disabled by default because
-//        many deployments do not require dynamic client registration.
-
-        http
-                // Redirect to the login page when not authenticated from the
-                // authorization endpoint
-                .exceptionHandling((exceptions) -> exceptions
-                        .defaultAuthenticationEntryPointFor(
-                        new LoginUrlAuthenticationEntryPoint("/login"),
-                        new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
-                    )
+        httpSecurity.getConfigurer(OAuth2AuthorizationServerConfigurer.class);
+        httpSecurity.exceptionHandling(
+                exception -> exception.authenticationEntryPoint(
+                        new LoginUrlAuthenticationEntryPoint("/login")
                 )
-                // terima access tokens untuk info user and/or client registration
-                .oauth2ResourceServer(
-                        (resourceServer) -> resourceServer.jwt(Customizer.withDefaults())
-                );
+        );
 
-        return http.build();
-    }
-
-    @Bean
-    @Order(2)
-    // authentication
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
-    throws Exception {
-        return http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated()
-                // Form login handles the redirect to the login page from the
-                // authorization server filter chain
-                ).formLogin(Customizer.withDefaults()).build();
-    }
-
-    @Bean
-    @Order(3)
-    // an instance of UserDetailsService for retrieving users to authenticate
-    public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withDefaultPasswordEncoder()
-                .username("Kristain")
-                .password("password")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
-    }
-
-    @Bean
-    // for managing client
-    public RegisteredClientRepository registeredClientRepository() {
-        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId("oidc-client")
-                .clientSecret("{noop}secret")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
-                .postLogoutRedirectUri("http://127.0.0.1:8080/")
-                .scope(OidcScopes.OPENID)
-                .scope(OidcScopes.PROFILE)
-                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-                .build();
-
-        return new InMemoryRegisteredClientRepository(oidcClient);
-    }
-
-    @Bean
-    // com.nimbusds.jose.jwk.source.JWKSource for signing access tokens
-    // inget ya pakai yang nimbuds kalau gak buildernya ga ada
-    public JWKSource<SecurityContext> jwkSource() {
-        KeyPair keyPair = generateRSAKey();
-        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-        RSAKey rsaKey = new RSAKey.Builder(publicKey)
-                .privateKey(privateKey)
-                .keyID(UUID.randomUUID().toString())
-                .build();
-        JWKSet jwkSet = new JWKSet(rsaKey);
-        return new ImmutableJWKSet<>(jwkSet);
+        return httpSecurity.build();
     }
 
 
-    //java.security.KeyPair with keys generated on startup used to create the JWKSource above.
-    private static KeyPair generateRSAKey() {
-        KeyPair keyPair;
-        try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-            keyPairGenerator.initialize(2048);
-            keyPair = keyPairGenerator.generateKeyPair();
-        } catch (Exception ex) {
-            throw new IllegalStateException(ex);
-        }
-        return keyPair;
-    }
-
-    @Bean
-    //An instance of JwtDecoder for decoding signed access tokens.
-    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
-        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
-    }
-
-    @Bean
-    //An instance of AuthorizationServerSettings to configure Spring Authorization Server
-    //  helps you configure genericcustomizations such as the endpoints the app exposes.
-    public AuthorizationServerSettings authorizationServerSettings() {
-        return AuthorizationServerSettings.builder().build();
-    }
 }
